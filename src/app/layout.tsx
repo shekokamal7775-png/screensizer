71a365e07d07cb280df988dd9c671b94c54e2288
shekokamal7775-png <shekokamal7775@gmail.com>
2026-05-21 15:52:12 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { Metadata } from 'next'
-import '../styles/tailwind.css'
+import '../../tailwind.css'
 import { Toaster } from 'sonner'
 
 export const metadata: Metadata = {
