dad034161ad45bf4f3a3c14954ff661dbe920d52
shekokamal7775-png <shekokamal7775@gmail.com>
2026-05-22 08:51:59 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-import { imageHosts } from './image-hosts.config.mjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: true,
-  distDir: process.env.DIST_DIR || '.next',
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  images: {
-    remotePatterns: imageHosts,
-    minimumCacheTTL: 60,
-  }
+  output: 'standalone',
 };
+
 export default nextConfig;
