3e49966252ad4114fd8308c6b228d092afeda9a8
shekokamal7775-png <shekokamal7775@gmail.com>
2026-05-22 08:59:12 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import type { Metadata } from 'next'
-import '../../tailwind.css'
-import { Toaster } from 'sonner'
-
-export const metadata: Metadata = {
-  title: 'ScreenSizer',
-  description: 'Screen size detector'
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-        <Toaster position="bottom-right" richColors />
-      </body>
+    <html lang="ar">
+      <body>{children}</body>
     </html>
-  )
+  );
 }
