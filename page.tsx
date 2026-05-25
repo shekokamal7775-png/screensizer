d1e57e648da0678f8b9b71ad233f2734e42639fc
shekokamal7775-png <shekokamal7775@gmail.com>
2026-05-25 13:08:46 +0300

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import PrivacyContent from './components/PrivacyContent';
-import PrivacyTOC from './components/PrivacyTOC';
+import PrivacyContent from '@/components/PrivacyContent';
+import PrivacyTOC from '@/components/PrivacyTOC';
 
 export const metadata: Metadata = {
   title: 'Privacy Policy | ScreenSizer',
